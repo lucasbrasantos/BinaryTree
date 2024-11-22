6b05adc865961c9c94abb3fe5fa5a3b82f7d14e4
Lucas Braga Santos <lucasbrasantos@gmail.com>
2024-11-22 17:28:29 -0300

    update
    Signed-off-by: Lucas Braga Santos <lucasbrasantos@gmail.com>

--- src/main/java/com/lucas/Main.java
+++ src/main/java/com/lucas/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
 
         int[] values = {1, 2, 3, 4, 5, 6, 7};
+        int[] values2 = {5, 11, 12, 14, 15, 18, 19, 21, 23, 25, 27, 28, 30, 32, 37};
         int[] randomValues = new int[20];
 
         Random random = new Random();
@@ -16,8 +17,8 @@ public class Main {
             randomValues[i] = random.nextInt(100);
         }
 
-        Tree mainTree = new Tree(randomValues);
-//        Tree mainTree = new Tree(values);
+//        Tree mainTree = new Tree(randomValues);
+        Tree mainTree = new Tree(values2);
         mainTree.printTree();
     }
 }
